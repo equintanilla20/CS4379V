8e8b35918491a93318c1d094aa2ead9b48085097
equintanilla20 <35775664+equintanilla20@users.noreply.github.com>
2023-01-23 23:38:07 -0600

    Completed hw1

--- src/hw1/1_4.cpp
+++ src/hw1/1_4.cpp
@@ -2,29 +2,43 @@
 #include <vector>
 #include <algorithm>
 // test input: 5 1 6 7 9 14 5 2 3 8 12 13
+// test input: 2 1 3 1 2
 using namespace std;
 
 double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
-    // int first[]  = {1, 6, 7,  9, 14};
-    // int second[] = {2, 3, 8, 12, 13};
-    // int merge[]  = {1, 2, 3,  6,  7, 8, 9, 13, 14};
-    int median;
+
     int m = nums1.size();
-    int mid1 = nums1.at(m/2);
-
     int n = nums2.size();
-    int mid2 = nums2.at(n/2);
+
+    if (m > n) // swap if nums1 is larger than nums2
+        return findMedianSortedArrays(nums2, nums1);
     
-    if (mid1 == mid2) {
-        median = (mid1+mid2) / 2;
-    } else if (mid1 < mid2) {
+    // Prep binary search
+    int start = 0;
+    int end = m;
+    int mergedMid = (m + n + 1) / 2; // Mid position of hypothetical merged array
 
-    } else {
+    while (start <= end) {
+        int partition1 = (start + end) / 2;
+        int partition2 = mergedMid - partition1;
 
+        // Check out of bounds
+        int leftMax1 = partition1 > 0 ? nums1[partition1 - 1] : INT_MIN;
+        int leftMax2 = partition2 > 0 ? nums2[partition2 - 1] : INT_MIN;
+        int rightMin1 = partition1 < m ? nums1[partition1] : INT_MAX;
+        int rightMin2 = partition2 < n ? nums2[partition2] : INT_MAX;
+
+        if (leftMax1 <= rightMin2 && leftMax2 <= rightMin1) {
+            if ((m + n) % 2 == 0)
+                return (max(leftMax1, leftMax2) + min(rightMin1, rightMin2)) / 2.0;
+            return max(leftMax1, leftMax2);
+        } else if (leftMax1 > rightMin2) {
+            end = partition1 - 1;
+        } else {
+            start = partition1 + 1;
+        }
     }
-
-    return median;
-
+    return 0;
 }
 
 int main() {
@@ -66,7 +80,7 @@ int main() {
         }
         secondList.push_back(numbersList2);
     }
-    findMedianSortedArrays(firstList, secondList);
-    // cout << "The median is " << findMedianSortedArrays(firstList, secondList) << endl;
+    // findMedianSortedArrays(firstList, secondList);
+    cout << "The median is " << findMedianSortedArrays(firstList, secondList) << endl;
     return 0;
 }
