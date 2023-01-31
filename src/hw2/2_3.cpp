e8852fb4946081c347ac41751432d714258daac4
equintanilla20 <35775664+equintanilla20@users.noreply.github.com>
2023-01-31 00:34:18 -0600

    Finished HW2

--- src/hw2/2_3.cpp
+++ src/hw2/2_3.cpp
@@ -6,31 +6,45 @@
 
 template<typename K, typename V>  // most generic way to set up a print function
 // can then be used with every map.  save it for later!!!!
-void print_map(std::unordered_map<K, V> const &m)
-{
-  for (auto const &pair: m) {
-    std::cout << "{" << pair.first << ": " << pair.second << "}\n";
-  }
+void print_map(std::unordered_map<K, V> const &m) {
+    for (auto const &pair: m) {
+        std::cout << "{" << pair.first << ": " << pair.second << "}\n";
+    }
 }
 
-int longestPal(std::string inString){
-   int maxLength = 0;
-   //// your code here ????
-   
-   return maxLength;
+int longestPal(std::string inString) {
+    std::unordered_map<char, int> chars;
+    int max = 0;
+    int odds = 0;
+
+    // Fill map with characters, count them as they enter
+    for (char ch : inString) {
+        if (chars.find(ch) == chars.end())
+            chars[ch] = 1;  // New char
+        else
+            chars[ch]++;    // Char already present
+    }
+
+    for (auto i = chars.begin(); i != chars.end(); i++) {
+        if (i->second % 2 == 0) {
+            max += i->second;
+        } else {
+            odds = 1;
+            max += i->second - 1;
+        }
+    }
+    return max + odds;
 }
-
-
 
 int main() {
-  // read in the string of characters
-  std::string inString = "";
-  //freopen("data.txt","r",stdin);
-  std::cin >> inString;
+    // read in the string of characters
+    std::string inString = "";
+    //freopen("data.txt","r",stdin);
+    std::cin >> inString;
 
-// call longestPal
- auto maxLength = longestPal(inString);
- // output
-  std::cout << maxLength << std::endl;
-  return 0;
+    // call longestPal
+    auto maxLength = longestPal(inString);
+    // output
+    std::cout << maxLength << std::endl;
+    return 0;
 }
