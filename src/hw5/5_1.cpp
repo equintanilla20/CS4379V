463d59ef7c6de7c04e6f368c2b223168c38c6609
equintanilla20 <35775664+equintanilla20@users.noreply.github.com>
2023-02-20 22:58:32 -0600

    Completed HW5

--- src/hw5/5_1.cpp
+++ src/hw5/5_1.cpp
@@ -4,8 +4,48 @@
 
 using namespace std;
 
+struct Job {
+    int startTime;
+    int endTime;
+    int profit;
+};
+
 int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-   //your code here
+    std::vector<Job> jobs;
+    std::vector<int> profits(startTime.size());
+
+    // Create Job structure based on vector info
+    for (size_t i = 0; i < startTime.size(); i++) {
+        jobs.push_back({startTime[i], endTime[i], profit[i]});
+    }
+
+    // Order by increasing start time
+    std::sort(jobs.begin(), jobs.end(), [](Job job1, Job job2) {
+        return job1.startTime < job2.startTime;
+    });
+
+    // For every element in the jobs vector fill in
+    // the profits vector to find the max combination
+    // of jobs
+    for (size_t i = 0; i < startTime.size(); i++) {
+        // Set current profit to 0 to help find max
+        profits[i] = 0;
+
+        // Check every other job for time collisions
+        for (size_t j = 0; j < i; j++) {
+            // If jobs don't collide time-wise and
+            // the other job has a higher profit
+            if (jobs[j].endTime <= jobs[i].startTime && profits[i] < profits[j]) {
+                // replace profits[i] with other job
+                profits[i] = profits[j];
+            }
+        }
+        // Add current job profit to profits vector
+        profits[i] += jobs[i].profit;
+    }
+    
+    // Return max element from the profits vector
+    return *max_element(profits.begin(), profits.end());
 }
 
 
--- src/hw5/5_2.cpp
+++ src/hw5/5_2.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 /*
     Given weights and profits of n items, and given a knapsack (container) of capacity 'W',
@@ -9,8 +11,95 @@
     Given two integer arrays Profits[0..n-1] and weights[0..n-1] which represent profits and weights
     associated with n items respectively and given an integer Knapsack_Capacity 'W' we need to return
     the maximum profit which can be obtained.
+
+    test input: 4 5 4 8 6 1 2 4 5 5
 */
 
+int findMaxWeight(std::vector<int> profits, std::vector<int> weights, int n, int w) {
+    // Matrix where rows correspond to weights in our knapsack
+    // Columns corresponds to the weight capacity of the knapsack
+    int table[n + 1][w + 1];
+    int prevMax, currentMax, weight, remainingCap;
+
+    // Base case of zero weights
+    for (int row = 0; row < (w + 1); row++) {
+        table[0][row] = 0;
+    }
+
+    // Base case of zero weight capacity
+    for (int col = 0; col < (n + 1); col++) {
+        table[col][0] = 0;
+    }
+
+    // Fill in the table
+    for (int i = 1; i <= n; i++) {
+        for (int capacity = 1; capacity <= w; capacity++) {
+            currentMax = 0;
+            // Save previous max profit for given capacity
+            prevMax = table[i - 1][capacity];
+
+            // Get weight of current weight
+            weight = weights[i - 1];
+
+            if (weight <= capacity) {
+                // If there's room in the knapsack
+                // Save current max based on the current weight's profit
+                // value + the value of the same number of items
+                // with the same remaining capacity in the table
+                remainingCap = capacity - weight;
+                currentMax = profits[i - 1] + table[i - 1][remainingCap];
+            }
+            // Save the max between the previous max and current max
+            table[i][capacity] = std::max(prevMax, currentMax);
+        }
+    }
+
+    // Value at this position in the table will be the overall max
+    return table[n][w];
+}
+
 int main() {
+    int sizeOfArrays, element, w;
+    std::vector<int> profits;
+    std::vector<int> weights;
+    std::cin >> sizeOfArrays;
+    int i = 0;
+
+    while (i < sizeOfArrays) {
+        std::cin >> element;
+        profits.push_back(element);
+        i++;
+    }
+
+    int j = 0;
+    while (j < sizeOfArrays) {
+        std::cin >> element;
+        weights.push_back(element);
+        j++;
+    }
+
+    std::cin >> w;
+
+    std::cout << findMaxWeight(profits, weights, sizeOfArrays, w) << std::endl;
+
     return 0;
 }
+
+/*
+    // Base Case, no weights or max weight is 0
+    if (n == 0 || w == 0)
+        return 0;
+ 
+    // If weight of the nth item is more
+    // than Knapsack capacity W, then
+    // this item cannot be included
+    // in the optimal solution
+    if (weights[n - 1] > w)
+        return findMaxWeight(profits, weights, n - 1, w);
+ 
+    // Return the maximum of two cases:
+    // (1) nth item included
+    // (2) not included
+    else
+        return std::max(profits[n - 1] + findMaxWeight(profits, weights, n - 1, w - weights[n - 1]), findMaxWeight(profits, weights, n - 1, w));
+*/
