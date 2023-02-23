c33173007e90c22df7598c3c2396fbb956974da4
equintanilla20 <35775664+equintanilla20@users.noreply.github.com>
2023-02-22 22:23:36 -0600

    pq testing

--- src/testing/minChange.cpp
+++ src/testing/minChange.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
-#include <map>
+#include <time.h>
+#include <queue>
 
-int minNumCntDP(int v, std::map<int, int>& min) {
-    if (v<0) return 0;
-    if (v==1) return 1;
+int main() {
+	auto comparator = [](const auto &a, const auto &b){
+		time_t current = time(NULL);
+		struct tm *tm_struct = localtime(&current);
+		int hour = tm_struct->tm_hour;
+		if (6 <= hour && hour < 18)
+			return a < b;
+		return a > b;
+	};
+
+	std::priority_queue<int, std::vector<int>, decltype(comparator)> pq(comparator);
+	
+	return 0;
 }
