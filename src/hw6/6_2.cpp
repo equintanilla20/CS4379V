67f53d4521d23915dddd9243d13f59637a3f0384
equintanilla20 <35775664+equintanilla20@users.noreply.github.com>
2023-03-01 20:20:29 -0600

    Finish HW6

--- src/hw6/6_2.cpp
+++ src/hw6/6_2.cpp
@@ -4,10 +4,28 @@
 
 using namespace std;
 // Test Case: 5 1 1 1 1 1 3
-// you can use a helper function here
+void calculate(vector<int>& arr, int target, int i, int sum, int &result) {
+    // "i" walks through array of ints
+    if (i == arr.size()) {
+        // If "i" reaches the end of the array
+        // check if the sum of the ints
+        // equals the target
+        if (sum == target) {
+            // If yes, this is a successful sequence
+            result++;
+        }
+    } else {
+        // Check the next num as a addition
+        calculate(arr, target, i+1, sum+arr[i], result);
+        // Check the next num as a subtraction
+        calculate(arr, target, i+1, sum-arr[i], result);
+    }
+}
 
-int findTargetSumWays(std::vector<int>& arr, int target) {
-   //your code here
+int findTargetSumWays(vector<int>& arr, int target) {
+    int result = 0;
+    calculate(arr, target, 0, 0, result);
+    return result;
 }
 
 int main() {
@@ -18,7 +36,7 @@ int main() {
     std::cin >> sizeOfArrays;
     int i = 0;
     
-    while(i < sizeOfArrays){
+    while (i < sizeOfArrays){
         std::cin >> element;
         vec.push_back(element);
         i++;
