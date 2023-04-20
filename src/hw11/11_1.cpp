39d29a525ac18165aa70b17509fd0b66af9f6c5f
equintanilla20 <35775664+equintanilla20@users.noreply.github.com>
2023-04-19 19:08:59 -0500

    Finished HW11

--- src/hw11/11_1.cpp
+++ src/hw11/11_1.cpp
@@ -1,87 +1,78 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 const int MAX = 100001;
 int nodes[MAX];
-int par1[MAX];
-int par2[MAX];
 
 // Test Case: 5 7 1 2 3 2 3 3 3 4 3 5 3 2 5 4 1 5 2 2 1 5 1
 // Test Case: 4 6 1 2 1 1 3 2 1 4 3 3 4 1 2 3 1 2 4 2
 
+struct hash_pair {
+    // Used to enable pairs in a set
+    std::size_t operator()(std::pair<int, std::pair<int, int>> const &pair) const {
+        return pair.second.first ^ pair.second.second;
+    }
+};
+
+// Initialize list of n nodes
 void initNodes(int n) {
     for (int i = 1; i <= n; i++) {
         nodes[i] = i;
     }
 }
 
-int find(int x) {
-    while(nodes[x] != x) {
+// Find root of node from graph
+int root(int x) {
+    while (nodes[x] != x) {
         nodes[x] = nodes[nodes[x]];
         x = nodes[x];
     }
     return x;
 }
 
+// Make union of two nodes that do not share root
 void makeUnion(int x, int y) {
     int p = root(x);
     int q = root(y);
-    nodes[x] = nodes[q];
-    std::cout << "Adding: (" << p << ", " << q << ")";
+    if (p != q) nodes[x] = nodes[q];
+    else return;
 }
 
-int findMSTEdges(int m, int n, std::pair<int, std::pair<int, int>> p[], int type) {
-    if (p[i] == type || p[i] == 3) {
-
+int findPath( int m, int n, std::pair<int, std::pair<int, int>> p[], std::set<std::pair<int, std::pair<int, int>>>& edgeSet, int type) {
+    int x, y, code, edgeCount = 0;
+    initNodes(n);   // Reset roots
+    for (int i = 0; i < m; i++)  {
+        x = p[i].second.first;
+        y = p[i].second.second;
+        code = p[i].first;
+        if (root(x) != root(y) && (code == type || code == 3)) {
+            makeUnion(x, y);
+            // Save edges to set for later
+            edgeSet.insert(p[i]);
+            edgeCount++;
+        }
     }
+    return edgeCount;
 }
 
 int three_types(int m, int n, std::pair<int, std::pair<int, int>> p[]) {
-	sort(p, p + m, greater<pair<int, pair<int, int>>>());
-    int cnt = 0, cnt1 = 0, cnt2 = 0;
+    // 1. Use Kruskal's Algorithm to find a minimum spanning tree
+    std::sort(p, p + m, std::greater<>());
+    std::set<std::pair<int, std::pair<int, int>>> edgeSet;
 
-    for (int i = 0; i < m; i++) {
-        if (p[i].first == 3 || ) {
-            int a = p[i].second.first, b = p[i].second.second;
-            int x = find1(a), y = find1(b);
+    // 2. Find count and add edges to set based on type
+    int menEdgeCount = findPath(m, n, p, edgeSet, 1);
+    int womenEdgeCount = findPath(m, n, p, edgeSet, 2);
 
-            if (x != y) {
-                cnt++;
-                merge1(x, y);
-            }
-
-            x = find2(a), y = find2(b);
-
-            if (x != y) {
-                cnt++;
-                merge2(x, y);
-            }
-        } else if (p[i].first == 2) {
-            int a = p[i].second.first, b = p[i].second.second;
-            int x = find2(a), y = find2(b);
-
-            if (x != y) {
-                cnt2++;
-                merge2(x, y);
-            }
-        } else {
-            int a = p[i].second.first, b = p[i].second.second;
-            int x = find1(a), y = find1(b);
-
-            if (x != y) {
-                cnt1++;
-                merge1(x, y);
-            }
-        }
+    // Return difference between total edges and unique edges needed to hit all 5 nodes
+    if (menEdgeCount == womenEdgeCount) {
+        return m - edgeSet.size();
     }
 
-    if (cnt / 2 + cnt1 == n - 1 && cnt / 2 + cnt2 == n - 1) {
-        return m - cnt / 2 - cnt1 - cnt2;
-    } else {
-        return -1;
-    }
+    return -1;
 }
 
 int main() {
@@ -89,12 +80,9 @@ int main() {
     std::cin >> n >> m; // input number of vertices and edges
     std::pair<int, std::pair<int, int>> p[MAX]; // array to store edges with their weights
     
-    int val[n + 1] = {0}; //array to store values associated with vertices
-    for (int i = 1; i <= n; i++) par1[i] = par2[i] = -1; //initializing parent arrays
-    
     for (int i = 0; i < m; i++) {
         int u, v, w;
-        std::cin >> u >> v >> w; //input edges and their weights
+        std::cin >> u >> v >> w;
         p[i] = {w, {u, v}};
     }
     
--- src/hw11/11_3.cpp
+++ src/hw11/11_3.cpp
@@ -4,10 +4,35 @@
 
 using namespace std;
 
-//your helper function here, if you have one
+// Test Case: 2 3 5 3 4 0 2 1 6 9 8 1 5 10 12 18 6 2 4 14 3
 
 int getMinPathCost(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
-    //your code here
+    int n = grid.size();
+    int m = grid[0].size();
+
+    // Initialize table for dynamic programming results
+    std::vector<std::vector<int>> table(n, std::vector<int>(m, 0));
+
+    // Begin filling table from the bottom
+    for (int i = 0; i < m; i++) {
+        table[n - 1][i] = grid[n - 1][i];
+    }
+
+    for (int i = n - 2; i >= 0; i--) {
+        // By row beginning with second from bottom
+        for (int j = 0; j < m; j++) {
+            // By column
+            int currentCost = INT_MAX;
+            for (int k = 0; k < m; k++) {
+                // Find move cost based on min previous result and current result
+                currentCost = std::min(currentCost, table[i + 1][k] + moveCost[grid[i][j]][k]);
+            }
+            // Add move cost to tile number into table
+            table[i][j] = currentCost + grid[i][j];
+        }
+    }
+
+    return *min_element(table[0].begin(), table[0].end());
 }
 
 int main() {
@@ -15,31 +40,31 @@ int main() {
     int m;
     int k;
     int element;
-    vector<vector<int>>grid;
-    vector<vector<int>>moveCost;
+    std::vector<std::vector<int>> grid;
+    std::vector<std::vector<int>> moveCost;
     
-    cin>>k;
-    cin>>n;
-    for(int i=0; i<n; i++){
-        vector<int>vec;
-        for(int j=0; j<k; j++){
+    std::cin >> k;
+    std::cin >> n;
+    for (int i = 0; i < n; i++){
+        std::vector<int> vec;
+        for (int j=0; j<k; j++){
             cin>>element;
             vec.push_back(element);
         }
         grid.push_back(vec);
     }
     
-    cin>>m;
-    for(int i=0; i<m; i++){
-        vector<int>vec;
-        for(int j=0; j<k; j++){
+    std::cin >> m;
+    for (int i=0; i<m; i++) {
+        std::vector<int> vec;
+        for(int j = 0; j < k; j++){
             cin>>element;
             vec.push_back(element);
         }
         moveCost.push_back(vec);
     }
     
-    cout<<getMinPathCost(grid, moveCost)<<endl;
+    std::cout << getMinPathCost(grid, moveCost) << std::endl;
 
     return 0;
 }
